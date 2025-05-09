2e1dec9a08ec9761ad0688bc9867e4ffe910be82
Gaurav051003 <136368404+Gaurav051003@users.noreply.github.com>
2025-05-10 00:54:14 +0530

    Update book.controller.js

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -9,3 +9,12 @@ export const getBook = async(req, res) => {
         res.status(500).json(error);
     }
 };
+export const getAllBook = async(req, res) => {
+    try {
+        const books = await Book.find();
+        res.status(200).json(books);
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json(error);
+    }
+};
