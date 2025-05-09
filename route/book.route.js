ffb5be1dbfdee14dc843766831da4239b62f738c
Gaurav051003 <136368404+Gaurav051003@users.noreply.github.com>
2025-05-10 00:52:59 +0530

    Update book.route.js

--- route/book.route.js
+++ route/book.route.js
@@ -4,6 +4,7 @@ import { getBook } from "../controller/book.controller.js";
 const router = express.Router();
 
 router.get("/", getBook);
+router.get("/all-books", getAllBook);
 
 
 export default router;
