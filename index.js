91fe8ab816d67f16225c1295943969e4cc8886ee
Gaurav051003 <gauravaryasirohi@gmail.com>
2025-05-09 23:39:28 +0530

    new routes add

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 app.use(cors({
-  origin: 'https://book-frontend-three.vercel.app' ,
+  origin: 'https://book-frontend-three.vercel.app',
   credentials: true
 }));
 app.use(express.json());
